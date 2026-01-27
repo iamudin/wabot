d320d109a90a17a81a6c894ea9d6944d592c06c9
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2026-01-27 13:18:18 +0700

    update

--- resources/views/surat/preview.blade.php
+++ resources/views/surat/preview.blade.php
@@ -1,7 +1,7 @@
 <div>
     <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
     <div class="p-4">
-        <iframe src="{{ $fileUrl }}" class="w-full h-96 border rounded-lg" frameborder="0">
+        <iframe src="https://docs.google.com/viewer?url={{ $fileUrl }}?time={{ time() }}&embedded=true" class="w-full h-96 border rounded-lg" frameborder="0">
         </iframe>
     </div>
 
