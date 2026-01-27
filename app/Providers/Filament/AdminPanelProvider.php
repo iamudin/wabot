fdf5ec32c7eccef64dfa2d0edd7aa2734a8686d2
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2026-01-27 15:01:20 +0700

    dfdsfsdf

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
     
         return $panel
             ->default()
+            ->spa()
             ->globalSearch(false)
             ->maxContentWidth(Width::Full)
             ->id('admin')
